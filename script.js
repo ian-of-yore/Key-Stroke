c89b69de44045beed090202a1604ba6c93d7be46
Ijaj Ahamed Nabil <37688324+ian-of-yore@users.noreply.github.com>
2022-09-14 22:22:44 +0600

    modal on center

--- script.js
+++ script.js
@@ -67,6 +67,7 @@ const gameOver = () => {
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
   const timeTaken = (finishTime - startTime) / 1000;
+  const timeTakenInt = parseInt(timeTaken);
 
   // show result modal
   resultModal.innerHTML = "";
@@ -79,7 +80,7 @@ const gameOver = () => {
   // show result
   resultModal.innerHTML += `
     <h1>Finished!</h1>
-    <p>You took: <span class="bold">${timeTaken}</span> seconds</p>
+    <p>You took: <span class="bold">${timeTakenInt}</span> seconds</p>
     <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
     <button onclick="closeModal()">Close</button>
   `;
